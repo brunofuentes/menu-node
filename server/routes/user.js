4aa14e2f82942b5cb275fbeaf29c6d3403ea0031
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-27 13:51:41 +0200

    rm unused package

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
 const { signUp, signIn, updateUser, getAllUsers, getSingleUser, deleteSingleUser, deleteAllUsers } = require('../controllers/user')
 
 router.post('/sign-up', signUp)
