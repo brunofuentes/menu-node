55b924eebaef02c0ee413cd4d18b5c9eb5404748
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-28 18:27:06 +0200

    basic navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,19 +1,15 @@
-import React from "react"
+import React from 'react'
+import Logout from './Logout'
 
 function Navbar() {
 	return (
-		<nav className="text-white font-semibold font-mono text-xl bg-gray-600 flex items-center justify-between py-5">
-			<div className="mx-2 sm:mx-5">Menu App</div>
+		<nav className="relative text-white font-semibold font-mono text-xl bg-gray-600 flex items-center justify-between py-5">
+			<div className="mx-2 sm:mx-5">
+				<a href="/">Menu App</a>
+			</div>
 			<div className="flex mx-2 sm:mx-5">
-				<a
-					href="/"
-					className="px-2">
+				<a href="/login" className="px-2">
 					Login
-				</a>
-				<a
-					href="/"
-					className="px-2">
-					Logout
 				</a>
 			</div>
 		</nav>
