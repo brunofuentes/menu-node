52ef0aeb6de7045e13c7e9f06263015f8cccbe8d
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-28 18:25:45 +0200

    add register-form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import Navbar from "./Navbar"
-import LoginForm from "./LoginForm"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
 import LoginForm from './LoginForm'
+import RegisterForm from './RegisterForm'
+import Dashboard from './Dashboard'
 
 function App() {
 	return (
 		<main>
-			<Navbar />
-			<LoginForm />
 			<Router>
 				<div className="relative">
 					<Navbar />
 				</div>
 				<Routes>
 					<Route path="/login" element={<LoginForm />} />
+					<Route path="/register" element={<RegisterForm />} />
 				</Routes>
 			</Router>
 		</main>
---
+++ client/src/RegisterForm.jsx
@@ -0,0 +1,122 @@
+import { useForm } from 'react-hook-form'
+
+function RegisterForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+	const onSubmit = (data) => {
+		if (data.password !== data.confirm_password) {
+			alert("Password doesn't match!")
+		} else {
+			fetch('/api/sign-up', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					username: data.username,
+					email: data.email,
+					password: data.password,
+				}),
+			})
+				.then((res) => {
+					res.json()
+					if (!res.ok) {
+						alert(res.statusText)
+					} else {
+						alert('Account created')
+					}
+				})
+				.catch((err) => alert('error, ', err))
+		}
+	}
+
+	return (
+		<div className="flex flex-col min-h-screen bg-gray-50 justify-center">
+			<div className="max-w-md w-full mx-auto">
+				<div className="text-3xl font-bold text-gray-900 mt-2 text-center">Create Account</div>
+			</div>
+			<div className="max-w-md w-full mx-auto mt-4 bg-white p-8 rounded border border-gray-300">
+				<form onSubmit={handleSubmit(onSubmit)} action="" className="space-y-6">
+					<div>
+						<label htmlFor="" className="text-sm font-bold text-gray-600 block">
+							Username
+						</label>
+						<input
+							{...register('username', {
+								required: true,
+								minLength: 5,
+								maxLength: 100,
+							})}
+							style={{ borderColor: errors.username ? 'red' : '' }}
+							type="text"
+							className="w-full p-2 border border-gray-300 rounded mt-1"
+						></input>
+						{errors.email && <span>Username is required!</span>}
+					</div>
+					<div>
+						<label htmlFor="" className="text-sm font-bold text-gray-600 block">
+							Email
+						</label>
+						<input
+							{...register('email', {
+								required: true,
+								minLength: 6,
+								maxLength: 100,
+							})}
+							style={{ borderColor: errors.email ? 'red' : '' }}
+							type="text"
+							className="w-full p-2 border border-gray-300 rounded mt-1"
+						></input>
+						{errors.email && <span>Email invalid</span>}
+					</div>
+					<div>
+						<label htmlFor="" className="text-sm font-bold text-gray-600 block">
+							Password
+						</label>
+						<input
+							{...register('password', { required: true })}
+							style={{
+								borderColor: errors.password ? 'red' : '',
+							}}
+							type="password"
+							className="w-full p-2 border border-gray-300 rounded mt-1"
+						></input>
+						{errors.password && <span>Invalid Password</span>}
+					</div>
+					<div>
+						<label htmlFor="" className="text-sm font-bold text-gray-600 block">
+							Cofirm Password
+						</label>
+						<input
+							{...register('confirm_password', { required: true })}
+							style={{
+								borderColor: errors.password ? 'red' : '',
+							}}
+							type="password"
+							className="w-full p-2 border border-gray-300 rounded mt-1"
+						></input>
+						{errors.password && <span>Invalid Password</span>}
+					</div>
+					<div className="flex items-center justify-between">
+						<div className="flex items-center">
+							<input type="checkbox" className="h-4 w-4 text-blue-300 rounded"></input>
+							<label htmlFor="" className="ml-2 text-sm text-gray-600">
+								Accept our terms and conditions
+							</label>
+						</div>
+					</div>
+					<div>
+						<button type="submit" className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white">
+							Register
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default RegisterForm
