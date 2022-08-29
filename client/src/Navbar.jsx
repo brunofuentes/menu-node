6a7b87a8d8f068bb61d427e007165a697545b623
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-29 23:09:39 +0200

    add Login,Logout buttons

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import Logout from './Logout'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+const userStatus = () => {
+	const token = localStorage.getItem('token')
+	if (token == null) return false
+	return true
+}
 
 function Navbar() {
+	let navigate = useNavigate()
+
+	const [isLogged, setIsLogged] = useState(() => userStatus())
+
+	const handleLogOut = () => {
+		localStorage.clear()
+		setIsLogged((prevIsLogged) => (prevIsLogged = false))
+		navigate('/')
+	}
+
+	const handleLogIn = () => {
+		navigate('/login')
+	}
+
 	return (
 		<nav className="relative text-white font-semibold font-mono text-xl bg-gray-600 flex items-center justify-between py-5">
 			<div className="mx-2 sm:mx-5">
 				<a href="/">Menu App</a>
 			</div>
 			<div className="flex mx-2 sm:mx-5">
-				<a href="/login" className="px-2">
-					Login
-				</a>
+				{isLogged ? (
+					<button onClick={handleLogOut} className="px-1">
+						Logout
+					</button>
+				) : (
+					<button onClick={handleLogIn} className="px-1">
+						Login
+					</button>
+				)}
 			</div>
 		</nav>
 	)
