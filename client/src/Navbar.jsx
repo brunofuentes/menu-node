ce91d67ccc6f9b615844864efb1f853077042e26
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-17 17:29:19 +0200

    add Dashboard button

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,39 +1,35 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
-
-const userStatus = () => {
-	const token = localStorage.getItem('token')
-	if (token == null) return false
-	return true
-}
+import UserContext from './UserContext'
+import { useContext } from 'react'
 
 function Navbar() {
+	const { isLogged, GetUserStatus, logoutUser } = useContext(UserContext)
+	GetUserStatus()
+
 	let navigate = useNavigate()
 
-	const [isLogged, setIsLogged] = useState(() => userStatus())
-
-	const handleLogOut = () => {
-		localStorage.clear()
-		setIsLogged((prevIsLogged) => (prevIsLogged = false))
-		navigate('/')
-	}
-
-	const handleLogIn = () => {
-		navigate('/login')
-	}
-
 	return (
-		<nav className="relative text-white font-semibold font-mono text-xl bg-gray-600 flex items-center justify-between py-5">
+		<nav className="relative text-white text-xl bg-gray-600 flex items-center justify-between py-5">
 			<div className="mx-2 sm:mx-5">
 				<a href="/">Menu App</a>
 			</div>
 			<div className="flex mx-2 sm:mx-5">
 				{isLogged ? (
-					<button onClick={handleLogOut} className="px-1">
-						Logout
-					</button>
+					<div>
+						<button onClick={() => navigate('/dashboard')}>Dashboard</button>
+						<button
+							onClick={() => {
+								logoutUser()
+								navigate('/')
+							}}
+							className="px-3"
+						>
+							Logout
+						</button>
+					</div>
 				) : (
-					<button onClick={handleLogIn} className="px-1">
+					<button onClick={() => navigate('/login')} className="px-1">
 						Login
 					</button>
 				)}
