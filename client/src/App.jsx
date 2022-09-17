a0ae007c5ebef11bac8b05b29b61ccb838816ed8
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-17 17:30:44 +0200

    add ContextAPI Providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,29 @@ import Home from './Home'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
 import Dashboard from './Dashboard'
+import RestaurantForm from './RestaurantForm'
+import { UserProvider } from './UserContext'
+import { RestaurantProvider } from './RestaurantContext'
 
 function App() {
 	return (
 		<main>
-			<Router>
-				<div className="relative">
-					<Navbar />
-				</div>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<LoginForm />} />
-					<Route path="/register" element={<RegisterForm />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-				</Routes>
-			</Router>
+			<UserProvider>
+				<RestaurantProvider>
+					<Router>
+						<div className="relative">
+							<Navbar />
+						</div>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/login" element={<LoginForm />} />
+							<Route path="/register" element={<RegisterForm />} />
+							<Route path="/dashboard" element={<Dashboard />} />
+							<Route path="/dashboard/edit-restaurant" element={<RestaurantForm />} />
+						</Routes>
+					</Router>
+				</RestaurantProvider>
+			</UserProvider>
 		</main>
 	)
 }
