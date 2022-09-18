32cab22fabd7e953e8d8066ae761d101e4fcac79
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-18 17:47:44 +0200

    refactor

--- server/models/item.js
+++ server/models/item.js
@@ -1,43 +1,46 @@
-'use strict';
+'use strict'
 
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Item extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-       // define association here
-    }
-  }
-  Item.init({
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false
-    },
-    section: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    imageUrl: {
-        type: DataTypes.STRING,
-    },
-    categories: DataTypes.ARRAY(DataTypes.STRING),
-    restaurant_id: {
-      type: DataTypes.INTEGER
-    },
-  }, {
-    sequelize,
-    modelName: 'Item',
-  });
-  return Item;
-};
+	class Item extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Item.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			description: {
+				type: DataTypes.STRING,
+			},
+			price: {
+				type: DataTypes.DECIMAL,
+				allowNull: false,
+			},
+			section: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			imageUrl: {
+				type: DataTypes.STRING,
+			},
+			categories: DataTypes.ARRAY(DataTypes.STRING),
+			restaurant_id: {
+				type: DataTypes.INTEGER,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Item',
+		}
+	)
+	return Item
+}
--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -1,51 +1,52 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Restaurant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // Restaurant.hasMany(models.Item, { foreignKey: 'restaurant_id' })
-    }
-  }
-  Restaurant.init({
-    name: {
-        type: DataTypes.STRING,
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    slug: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    address: {
-      type: DataTypes.STRING,
-    },
-    phone: {
-        type: DataTypes.STRING,
-    },
-    imageUrl: {
-        type: DataTypes.STRING,
-    },
-    websiteUrl: {
-        type: DataTypes.STRING,
-    },
-    instagramUrl: {
-        type: DataTypes.STRING,
-    },
-    facebookUrl: {
-        type: DataTypes.STRING,
-    }
-  }, {
-    sequelize,
-    modelName: 'Restaurant',
-  });
-  return Restaurant;
-};
+	class Restaurant extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// Restaurant.hasMany(models.Item, { foreignKey: 'restaurant_id' })
+		}
+	}
+	Restaurant.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+			},
+			description: {
+				type: DataTypes.STRING,
+			},
+			slug: {
+				type: DataTypes.STRING,
+				unique: true,
+				allowNull: false,
+			},
+			address: {
+				type: DataTypes.STRING,
+			},
+			phone: {
+				type: DataTypes.STRING,
+			},
+			imageUrl: {
+				type: DataTypes.STRING,
+			},
+			websiteUrl: {
+				type: DataTypes.STRING,
+			},
+			instagramUrl: {
+				type: DataTypes.STRING,
+			},
+			facebookUrl: {
+				type: DataTypes.STRING,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Restaurant',
+		}
+	)
+	return Restaurant
+}
