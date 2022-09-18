f74dadcb08e97ca78baa194b55f521afe646a343
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-18 17:48:17 +0200

    rm unused props

--- client/src/RestaurantData.jsx
+++ client/src/RestaurantData.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router'
 import RestaurantContext from './RestaurantContext'
 
-function RestaurantData({ props }) {
+function RestaurantData() {
 	const navigate = useNavigate()
 	const { restaurant, getRestaurantData } = useContext(RestaurantContext)
 	useEffect(() => {
