3343b3d1e2e43c8db4083717ed4f059c44f1789f
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-18 17:47:30 +0200

    add get and update routes to item

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { createItem, getAllItems, getItemsRestaurant, deleteItem } = require('../controllers/item')
+const { createItem, updateItem, getAllItems, getItemsRestaurant, deleteItem, getItem } = require('../controllers/item')
 
 router.post('/items', createItem)
 
+router.patch('/items/:id', updateItem)
+
 router.get('/items', getAllItems)
+
+router.get('/items/:id', getItem)
 
 router.get('/:id/items', getItemsRestaurant)
 
