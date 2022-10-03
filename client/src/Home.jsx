1e03b0584a5d4eecaad3a9d6a556fce1422c464b
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-03 17:25:34 -0300

    min-screen change

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
 	return (
-		<div className="min-h-screen flex items-center justify-center text-3xl font-bold font-mono">
+		<div className="my-10 flex items-center justify-center text-3xl font-bold font-mono">
 			<h1>Welcome to Menu App!</h1>
 		</div>
 	)
