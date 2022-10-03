f924fe61c4300c1f2238e7c3ba7d9a4ed9d0f5e3
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-03 17:26:07 -0300

    add AdminPageLayout + navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './Navbar'
 import Home from './Home'
+import Menu from './Menu'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
 import Dashboard from './Dashboard'
 import RestaurantForm from './RestaurantForm'
 import MenuItemForm from './MenuItemForm'
+import AdminPageLayout from './AdminPageLayout'
 import { UserProvider } from './UserContext'
 import { RestaurantProvider } from './RestaurantContext'
 
@@ -15,17 +16,17 @@ function App() {
 			<UserProvider>
 				<Router>
 					<RestaurantProvider>
-						<div className="relative">
-							<Navbar />
-						</div>
 						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/login" element={<LoginForm />} />
-							<Route path="/register" element={<RegisterForm />} />
-							<Route path="/dashboard" element={<Dashboard />} />
-							<Route path="/dashboard/edit-restaurant" element={<RestaurantForm />} />
-							<Route path="/dashboard/edit-item" element={<MenuItemForm />} />
-							<Route path="/dashboard/add-item" element={<MenuItemForm />} />
+							<Route path="/menu" element={<Menu />} />
+							<Route element={<AdminPageLayout />}>
+								<Route path="/" element={<Home />} />
+								<Route path="/login" element={<LoginForm />} />
+								<Route path="/register" element={<RegisterForm />} />
+								<Route path="/dashboard" element={<Dashboard />} />
+								<Route path="/dashboard/edit-restaurant" element={<RestaurantForm />} />
+								<Route path="/dashboard/edit-item" element={<MenuItemForm />} />
+								<Route path="/dashboard/add-item" element={<MenuItemForm />} />
+							</Route>
 						</Routes>
 					</RestaurantProvider>
 				</Router>
