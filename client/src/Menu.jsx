92e178a36b59ae862119f9b9d9cde93de1ba3888
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-04 09:56:54 -0300

    get data from MenuContext

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -1,25 +1,26 @@
 import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 import { useEffect } from 'react'
-import RestaurantContext from './RestaurantContext'
+import MenuContext from './MenuContext'
 
 function Menu() {
-	const { items, restaurant, getMenuData, getRestaurantData } = useContext(RestaurantContext)
+	const { slug } = useParams()
+	const { restaurant, menuItems, getMenuRestaurantData } = useContext(MenuContext)
 
 	useEffect(() => {
-		getMenuData()
-		getRestaurantData()
+		getMenuRestaurantData(slug)
 	}, [])
 
 	let sections = []
-	items.map((item) => (sections.includes(item.section) ? null : sections.push(item.section)))
+	menuItems.map((item) => (sections.includes(item.section) ? null : sections.push(item.section)))
 
 	return (
 		<main>
 			<section id="restaurant-header">
-				<img src={restaurant.imageUrl} className="max-h-[450px] w-full object-cover" alt="Restaurant Cover" />
+				<img src={restaurant?.imageUrl} className="max-h-[450px] w-full object-cover" alt="Restaurant Cover" />
 				<div className="mx-auto max-w-2xl px-4 py-8">
-					<h1 className="font-bold text-4xl">{restaurant.name}</h1>
-					<p>{restaurant.description}</p>
+					<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
+					<p>{restaurant?.description}</p>
 				</div>
 			</section>
 			{sections.map((section, section_index) => {
@@ -27,18 +28,21 @@ function Menu() {
 					<div className="sm:w-1/2 mx-auto" key={section_index}>
 						<h2 className="font-bold text-2xl mt-4 pt-4">{section}</h2>
 						<div>
-							{items.map(
+							{menuItems.map(
 								(item, item_index) =>
 									item.section === section && (
 										<div key={item_index} className="flex border-t pt-4">
 											<div className="mr-auto flex flex-col pr-4">
 												<p>{item.name}</p>
 												<p>{item.description}</p>
+												<p>
+													{item.categories.map((category) => (
+														<span className="mx-0.5 p-0.5 text-sm bg-gray-300 rounded-md">{category}</span>
+													))}
+												</p>
 												<p>R${item.price}</p>
 											</div>
-											<div className="">
-												<img className="h-24 w-40 flex-none rounded object-cover" src={item.imageUrl} alt={item.name} />
-											</div>
+											<img className="h-24 w-40 flex-none rounded object-cover" src={item.imageUrl} alt={item.name} />
 										</div>
 									)
 							)}
