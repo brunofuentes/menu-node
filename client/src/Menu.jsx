9010eacb78549cf41241e2c3ac024335c96b0095
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-05 19:24:17 -0300

    add footer section

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -16,8 +16,8 @@ function Menu() {
 
 	return (
 		<main>
-			<section id="restaurant-header">
-				<img src={restaurant?.imageUrl} className="max-h-[450px] w-full object-cover" alt="Restaurant Cover" />
+			<section id="restaurant-header" className="w-full mx-auto">
+				<img src={restaurant?.imageUrl} className="max-h-[450px] object-cover w-full" alt="Restaurant Cover" />
 				<div className="mx-auto max-w-2xl px-4 py-8">
 					<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
 					<p>{restaurant?.description}</p>
@@ -25,19 +25,21 @@ function Menu() {
 			</section>
 			{sections.map((section, section_index) => {
 				return (
-					<div className="sm:w-1/2 mx-auto" key={section_index}>
+					<section className="sm:w-1/2 mx-auto px-3" key={section_index}>
 						<h2 className="font-bold text-2xl mt-4 pt-4">{section}</h2>
 						<div>
 							{menuItems.map(
 								(item, item_index) =>
 									item.section === section && (
 										<div key={item_index} className="flex border-t pt-4">
-											<div className="mr-auto flex flex-col pr-4">
-												<p>{item.name}</p>
-												<p>{item.description}</p>
+											<div className="mr-auto flex flex-col pr-3">
+												<p className="font-medium text-lg pb-2">{item.name}</p>
+												<p className="py-1">{item.description}</p>
 												<p>
-													{item.categories.map((category) => (
-														<span className="mx-0.5 p-0.5 text-sm bg-gray-300 rounded-md">{category}</span>
+													{item.categories.map((category, cat_index) => (
+														<span key={cat_index} className="mx-1 text-sm bg-yellow-100 ">
+															<small>{category}</small>
+														</span>
 													))}
 												</p>
 												<p>R${item.price}</p>
@@ -47,9 +49,35 @@ function Menu() {
 									)
 							)}
 						</div>
-					</div>
+					</section>
 				)
 			})}
+			<section className="px-3 pb-5">
+				<div className="text-center text-gray-500 border-t">
+					<p className=" mt-5 py-2">
+						{' '}
+						Como podemos melhorar? Deixe-nos uma sugestão{' '}
+						<a className="underline" href="#">
+							aqui
+						</a>
+					</p>
+					<div className="flex justify-center my-1">
+						<a className="px-1" href={restaurant?.instagramUrl}>
+							<img src="/images/icons/instagram_icon.svg" alt="Link to Instagram" />
+						</a>
+						<a className="px-1" href={restaurant?.facebookUrl}>
+							<img src="/images/icons/facebook_icon.svg" alt="Link to Facebook" />
+						</a>
+					</div>
+					<p className="py-1">
+						Feito com ❤️ por{' '}
+						<a href="https://menuonline.app" target="_blank" rel="noreferrer">
+							menuonline.app
+						</a>
+					</p>
+					<p className="py-1">Copyright ⓒ {new Date().getFullYear()}</p>
+				</div>
+			</section>
 		</main>
 	)
 }
