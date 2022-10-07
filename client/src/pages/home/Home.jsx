ae9be2070def2f5a98edb557dcea7763ff1f41ca
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-07 15:33:23 -0300

    add btn menu example

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function Home() {
 	return (
-		<div className="my-10 flex items-center justify-center text-3xl font-bold font-mono">
-			<h1>Welcome to Menu App!</h1>
-		</div>
+		<>
+			<div className="my-10 flex flex-col items-center justify-center">
+				<h1 className="font-bold text-3xl">Bem vindo ao Menu Online!</h1>
+				<div className="my-5">
+					<p>O menu online é uma aplicação web para restaurantes criarem e gerenciarem seus próprios cardápios digitais.</p>
+				</div>
+				<a
+					href="/padoca-veronese/menu"
+					className="bg-slate-500 text-white font-medium rounded-2xl p-3 hover:bg-slate-400 transition-color duration-200 transform hover:scale-110"
+				>
+					Cardápio de Exemplo
+				</a>
+			</div>
+		</>
 	)
 }
 
