91582e006e5283668c7aaafa57927e80dc51aa2b
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-07 18:47:05 -0300

    add qr-code to homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,14 +5,15 @@ function Home() {
 		<>
 			<div className="my-10 flex flex-col items-center justify-center">
 				<h1 className="font-bold text-3xl">Bem vindo ao Menu Online!</h1>
-				<div className="my-5">
+				<div className="px-3 text-center my-10">
 					<p>O menu online é uma aplicação web para restaurantes criarem e gerenciarem seus próprios cardápios digitais.</p>
 				</div>
 				<a
 					href="/padoca-veronese/menu"
-					className="bg-slate-500 text-white font-medium rounded-2xl p-3 hover:bg-slate-400 transition-color duration-200 transform hover:scale-110"
+					className="bg-slate-500 text-white text-center font-medium rounded-2xl p-3 hover:bg-slate-400 transition-color duration-200 transform hover:scale-110"
 				>
-					Cardápio de Exemplo
+					<div className="p-1">Cardápio de Exemplo</div>
+					<img width={200} height={200} src="/images/qrcode-padoca-veronese.png" alt="" />
 				</a>
 			</div>
 		</>
