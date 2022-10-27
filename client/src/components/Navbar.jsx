2c7f26b6961c77d242faa98b19937ec67859ecef
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-10-27 18:03:31 -0300

    changed buttons to Links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 import UserContext from './../context/UserContext'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
 	const { isLogged, GetUserStatus, logoutUser } = useContext(UserContext)
@@ -12,12 +13,12 @@ function Navbar() {
 	return (
 		<nav className="sticky text-white text-xl bg-gray-600 flex items-center justify-between py-5">
 			<div className="mx-2 sm:mx-5">
-				<a href="/">Menu App</a>
+				<Link to="/">Menu App</Link>
 			</div>
 			<div className="flex mx-2 sm:mx-5">
 				{isLogged ? (
 					<div>
-						<button onClick={() => navigate('/dashboard')}>Dashboard</button>
+						<Link to="/dashboard">Dashboard</Link>
 						<button
 							onClick={() => {
 								logoutUser()
