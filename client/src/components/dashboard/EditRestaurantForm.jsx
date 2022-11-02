9e138d8a65cc12898cddda2f2524aa9922949f86
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-02 18:28:46 -0300

    code refactor

--- client/src/components/dashboard/EditRestaurantForm.jsx
+++ client/src/components/dashboard/EditRestaurantForm.jsx
@@ -157,7 +157,10 @@ function RestaurantForm() {
 						{errors.facebookUrl}
 					</div>
 					<div>
-						<button type="submit" className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white">
+						<button
+							type="submit"
+							className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white"
+						>
 							Save
 						</button>
 					</div>
