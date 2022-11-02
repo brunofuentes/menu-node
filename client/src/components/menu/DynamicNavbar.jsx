41a19c23d303aab0a82d1641b91e4dc29d561323
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-02 17:19:02 -0300

    feat: add dynamic navbar to MenuPage

---
+++ client/src/components/menu/DynamicNavbar.jsx
@@ -0,0 +1,90 @@
+import React, { useRef, useState, useEffect } from 'react'
+
+const useHeadingsData = () => {
+	const [nestedHeadings, setNestedHeadings] = useState([])
+
+	useEffect(() => {
+		const elements = document.querySelectorAll('h2')
+		const headings = Array.from(elements).map((heading) => {
+			return {
+				id: heading.id,
+				title: heading.innerText,
+			}
+		})
+
+		setNestedHeadings(headings)
+	}, [])
+
+	return { nestedHeadings }
+}
+
+const useIntersectionObserver = (setActiveId) => {
+	const headingElementsRef = useRef({})
+
+	useEffect(() => {
+		const callback = (headings) => {
+			headingElementsRef.current = headings.reduce((map, headingElement) => {
+				map[headingElement.target.id] = headingElement
+				return map
+			}, headingElementsRef.current)
+
+			const visibleHeadings = []
+			Object.keys(headingElementsRef.current).forEach((key) => {
+				const headingElement = headingElementsRef.current[key]
+				if (headingElement.isIntersecting) visibleHeadings.push(headingElement)
+			})
+
+			const getIndexFromId = (id) => headingElements.findIndex((heading) => heading.id === id)
+
+			if (visibleHeadings.length === 1) {
+				setActiveId(visibleHeadings[0].target.id)
+			} else if (visibleHeadings.length > 1) {
+				const sortedVisibleHeadings = visibleHeadings.sort((a, b) =>
+					getIndexFromId(a.target.id) > getIndexFromId(b.target.id) ? 1 : -1
+				)
+				setActiveId(sortedVisibleHeadings[0].target.id)
+			}
+		}
+
+		const observer = new IntersectionObserver(callback, {
+			rootMargin: '0px 0px -40% 0px',
+		})
+
+		const headingElements = Array.from(document.querySelectorAll('h2'))
+		headingElements.forEach((element) => observer.observe(element))
+
+		return () => observer.disconnect()
+	}, [setActiveId])
+}
+
+const DynamicNavbar = () => {
+	const [activeId, setActiveId] = useState()
+	const { nestedHeadings } = useHeadingsData()
+
+	useIntersectionObserver(setActiveId)
+
+	return (
+		<div className="container sticky top-0 mx-auto bg-white py-1">
+			<nav className="mx-auto flex max-w-2xl gap-4 px-4">
+				{nestedHeadings.map((heading) => {
+					const activeClasses = heading.id === activeId ? 'bg-black text-white' : ''
+					return (
+						<button
+							key={heading.title}
+							className={`rounded-full border border-black py-1 px-4 text-sm ${activeClasses}`}
+							onClick={() => {
+								document.querySelector(`#${heading.id}`).scrollIntoView({
+									behavior: 'smooth',
+								})
+							}}
+						>
+							{heading.title}
+						</button>
+					)
+				})}
+			</nav>
+		</div>
+	)
+}
+
+export default DynamicNavbar
--- client/src/pages/menu/MenuPage.jsx
+++ client/src/pages/menu/MenuPage.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
 import MenuContext from '../../context/MenuContext'
+import DynamicNavbar from '../../components/menu/DynamicNavbar'
 
 function Menu() {
 	const { slug } = useParams()
@@ -9,77 +9,95 @@ function Menu() {
 
 	useEffect(() => {
 		getMenuRestaurantData(slug)
-	}, [])
+	}, [slug])
 
 	let sections = []
 	menuItems.map((item) => (sections.includes(item.section) ? null : sections.push(item.section)))
 
-	return (
-		<main>
-			<section id="restaurant-header" className="w-full mx-auto">
-				<img src={restaurant?.imageUrl} className="max-h-[450px] object-cover w-full sm:w-1/2 mx-auto" alt="Restaurant Cover" />
-				<div className="mx-auto max-w-2xl px-4 py-8">
-					<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
-					<p>{restaurant?.description}</p>
-				</div>
-			</section>
-			{sections.map((section, section_index) => {
-				return (
-					<section className="sm:w-1/2 mx-auto px-3" key={section_index}>
-						<h2 className="font-bold text-2xl mt-4 pt-4">{section}</h2>
-						<div>
-							{menuItems.map(
-								(item, item_index) =>
-									item.section === section && (
-										<div key={item_index} className="flex border-t pt-4">
-											<div className="mr-auto flex flex-col pr-3">
-												<p className="font-medium text-lg pb-2">{item.name}</p>
-												<p className="py-1">{item.description}</p>
-												<p>
-													{item.categories.map((category, cat_index) => (
-														<span key={cat_index} className="mx-1 text-sm bg-yellow-100 ">
-															<small>{category}</small>
-														</span>
-													))}
-												</p>
-												<p>R${item.price}</p>
-											</div>
-											<img className="h-24 w-40 flex-none rounded object-cover" src={item.imageUrl} alt={item.name} />
-										</div>
-									)
-							)}
-						</div>
-					</section>
-				)
-			})}
-			<section className="px-3 pb-5">
-				<div className="text-center text-gray-500 border-t">
-					<p className=" mt-5 py-2">
-						{' '}
-						Como podemos melhorar? Deixe-nos uma sugestão{' '}
-						<a className="underline" href="#">
-							aqui
-						</a>
-					</p>
-					<div className="flex justify-center my-1">
-						<a className="px-1" href={restaurant?.instagramUrl}>
-							<img src="/images/icons/instagram_icon.svg" alt="Link to Instagram" />
-						</a>
-						<a className="px-1" href={restaurant?.facebookUrl}>
-							<img src="/images/icons/facebook_icon.svg" alt="Link to Facebook" />
-						</a>
+	if (sections.length < 1) {
+		return (
+			<div className="sm:w-1/2 mx-auto px-3 flex flex-col justify-center items-center min-h-screen">
+				<span className="">Loading...</span>
+			</div>
+		)
+	} else {
+		return (
+			<main className="sm:w-1/2 mx-auto px-3">
+				<section id="restaurant-header" className="w-full mx-auto">
+					<img src={restaurant?.imageUrl} className="object-cover w-full mx-auto" alt="Restaurant Cover" />
+					<div className="mx-auto max-w-2xl px-4 py-8">
+						<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
+						<p>{restaurant?.description}</p>
 					</div>
-					<p className="py-1">
-						Feito com ❤️ por{' '}
-						<a href="https://menuonline.app" target="_blank" rel="noreferrer">
-							menuonline.app
-						</a>
-					</p>
-					<p className="py-1">Copyright ⓒ {new Date().getFullYear()}</p>
-				</div>
-			</section>
-		</main>
-	)
+				</section>
+				<DynamicNavbar />
+				{sections.map((section, section_index) => {
+					return (
+						<section key={section_index}>
+							<h2 id={section.replace(' ', '-').toLowerCase()} className="font-bold text-2xl mt-4 pt-4">
+								{section}
+							</h2>
+							<div>
+								{menuItems.map(
+									(item, item_index) =>
+										item.section === section && (
+											<div key={item_index} className="flex border-t pt-4">
+												<div className="mr-auto flex flex-col pr-3">
+													<p className="font-medium text-lg pb-2">{item.name}</p>
+													<p className="py-1">{item.description}</p>
+													<p>
+														{item.categories.map((category, cat_index) => (
+															<span
+																key={cat_index}
+																className="mx-1 text-sm bg-yellow-100 "
+															>
+																<small>{category}</small>
+															</span>
+														))}
+													</p>
+													<p>R${item.price}</p>
+												</div>
+												<img
+													className="h-24 w-40 flex-none rounded object-cover"
+													src={item.imageUrl}
+													alt={item.name}
+												/>
+											</div>
+										)
+								)}
+							</div>
+						</section>
+					)
+				})}
+				<section className="px-3 pb-5">
+					<div className="text-center text-gray-500 border-t">
+						<p className=" mt-5 py-2">
+							{' '}
+							Como podemos melhorar? Deixe-nos uma sugestão{' '}
+							<a className="underline" href="#">
+								aqui
+							</a>
+						</p>
+						<div className="flex justify-center my-1">
+							<a className="px-1" href={restaurant?.instagramUrl}>
+								<img src="/images/icons/instagram_icon.svg" alt="Link to Instagram" />
+							</a>
+							<a className="px-1" href={restaurant?.facebookUrl}>
+								<img src="/images/icons/facebook_icon.svg" alt="Link to Facebook" />
+							</a>
+						</div>
+						<p className="py-1">
+							Feito com ❤️ por{' '}
+							<a href="https://menuonline.app" target="_blank" rel="noreferrer">
+								menuonline.app
+							</a>
+						</p>
+						<p className="py-1">Copyright ⓒ {new Date().getFullYear()}</p>
+					</div>
+				</section>
+			</main>
+		)
+	}
 }
 
 export default Menu
