8f5ff07764b1b84d100ff70d4ae91103acd1c2ca
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-03 18:50:39 -0300

    feat: add btn 'voltar' to EditItemPage

--- client/src/pages/dashboard/EditItemPage.jsx
+++ client/src/pages/dashboard/EditItemPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import EditItemForm from '../../components/dashboard/EditItemForm'
 
 function EditItemPage() {
 	return (
 		<div>
+			<Link className="inline-block font-medium" to="/dashboard/menu">
+				<div className="flex items-center">
+					<img
+						height="16px"
+						width="16px"
+						src="/images/icons/chevron_left.svg"
+						alt="voltar para página dos items do menu"
+					/>
+					<span>Voltar</span>
+				</div>
+			</Link>
 			<EditItemForm />
 		</div>
 	)
