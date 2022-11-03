b92a3f92adb7a97aba07dff7be3d58159da52368
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-03 18:57:48 -0300

    feat: wip DashboardPage

--- client/src/pages/dashboard/DashboardPage.jsx
+++ client/src/pages/dashboard/DashboardPage.jsx
@@ -1,40 +1,14 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import ItemsTable from '../../components/dashboard/ItemsTable'
-import QRCodeData from '../../components/dashboard/QRCodeData'
-import RestaurantData from '../../components/dashboard/RestaurantData'
+import { useContext } from 'react'
+import UserContext from '../../context/UserContext'
 
 function DashboardPage() {
-	const navigate = useNavigate()
-	const token = sessionStorage.getItem('token')
-
-	useEffect(() => {
-		fetch('/api/protected', {
-			method: 'GET',
-			headers: {
-				Authorization: token,
-			},
-		})
-			.then((res) => res.json())
-			.then((data) => {})
-			.catch((err) => {
-				navigate('/login')
-				console.error('Error: ' + err)
-			})
-	}, [token, navigate])
+	const { user } = useContext(UserContext)
 
 	return (
-		<div className="w-4/5 mx-auto">
-			<div className="flex-col flex items-center justify-center text-3xl py-5">
-				<h1>Dashboard</h1>
-			</div>
-			<div className="text-center font-md"></div>
-			<div className="p-3 flex">
-				<RestaurantData />
-				<QRCodeData />
-			</div>
-			<ItemsTable />
-		</div>
+		<>
+			<h1 className="text-3xl font-medium">Hello, {user?.firstName || user?.username}</h1>
+		</>
 	)
 }
 
