8ddde3743d2545fd1a80771ba00dcefcd94048d8
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-03 19:40:09 -0300

    fix: qr-code homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useContext } from 'react'
+import UserContext from '../../context/UserContext'
 
 function Home() {
 	return (
@@ -6,15 +9,15 @@ function Home() {
 			<div className="my-10 flex flex-col items-center justify-center">
 				<h1 className="font-bold text-3xl">Bem vindo ao Menu Online!</h1>
 				<div className="px-3 text-center my-10">
-					<p>O menu online é uma aplicação web para restaurantes criarem e gerenciarem seus próprios cardápios digitais.</p>
+					<p>
+						O menu online é uma aplicação web para restaurantes criarem e gerenciarem seus próprios
+						cardápios digitais.
+					</p>
 				</div>
-				<a
-					href="/padoca-veronese/menu"
-					className="bg-slate-500 text-white text-center font-medium rounded-2xl p-3 hover:bg-slate-400 transition-color duration-200 transform hover:scale-110"
-				>
+				<div className="bg-slate-500 text-white text-center font-medium rounded-2xl p-3 hover:bg-slate-400 transition-color duration-200 transform hover:scale-110">
 					<div className="p-1">Cardápio de Exemplo</div>
-					<img width={200} height={200} src="/images/qrcode-padoca-veronese.png" alt="" />
-				</a>
+					<img width={200} height={200} src="/images/pacoda-veronese-vercel.png" alt="" />
+				</div>
 			</div>
 		</>
 	)
