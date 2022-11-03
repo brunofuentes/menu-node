2ab194088327dec5bd1a73940a3a715038efffa3
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-03 15:19:28 -0300

    feat: new paths (admin pages redesign: sidebar)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { UserProvider } from './context/UserContext'
+import { RestaurantProvider } from './context/RestaurantContext'
+import { MenuProvider } from './context/MenuContext'
 import Home from './pages/home/Home'
 import MenuPage from './pages/menu/MenuPage'
 import LoginPage from './pages/auth/LoginPage'
@@ -7,9 +10,9 @@ import DashboardPage from './pages/dashboard/DashboardPage'
 import EditRestaurantPage from './pages/dashboard/EditRestaurantPage'
 import EditItemPage from './pages/dashboard/EditItemPage'
 import RegisterPage from './pages/auth/RegisterPage'
-import { UserProvider } from './context/UserContext'
-import { RestaurantProvider } from './context/RestaurantContext'
-import { MenuProvider } from './context/MenuContext'
+import RestaurantData from './components/dashboard/RestaurantData'
+import ItemsTable from './components/dashboard/ItemsTable'
+import QRCodeData from './components/dashboard/QRCodeData'
 
 function App() {
 	return (
@@ -24,9 +27,12 @@ function App() {
 								<Route path="/login" element={<LoginPage />} />
 								<Route path="/register" element={<RegisterPage />} />
 								<Route path="/dashboard" element={<DashboardPage />} />
+								<Route path="/dashboard/restaurant" element={<RestaurantData />} />
 								<Route path="/dashboard/edit-restaurant" element={<EditRestaurantPage />} />
+								<Route path="/dashboard/menu" element={<ItemsTable />} />
 								<Route path="/dashboard/edit-item/:id" element={<EditItemPage />} />
 								<Route path="/dashboard/add-item" element={<EditItemPage />} />
+								<Route path="/dashboard/qr-code" element={<QRCodeData />} />
 							</Route>
 						</Routes>
 					</MenuProvider>
