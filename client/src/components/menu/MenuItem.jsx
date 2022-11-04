6665f0453b28e68091576debbea10f92455398d8
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-04 16:23:34 -0300

    style: design changes on MenuPage/MenuItem

---
+++ client/src/components/menu/MenuItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function MenuItem(props) {
+	const { item } = props
+
+	return (
+		<div className="border-t border-gray-300 mt-2">
+			<div className="flex justify-between">
+				<span className="font-medium text-lg">{item.name}</span>
+			</div>
+			<div className="flex">
+				<div className="mr-auto flex flex-col pr-3">
+					<p className="py-1 text-xs">{item.description}</p>
+					<span>R${item.price}</span>
+					<p>
+						{item.categories.map((category, cat_index) => (
+							<span key={cat_index} className="mx-1 text-sm bg-yellow-100 px-0.5 rounded-md">
+								<small>{category}</small>
+							</span>
+						))}
+					</p>
+				</div>
+				<div className="flex flex-none items-center justify-center">
+					<img
+						height="24"
+						width="40"
+						className="h-24 w-40 rounded object-cover"
+						src={item.imageUrl}
+						alt={item.name}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default MenuItem
--- client/src/pages/menu/MenuPage.jsx
+++ client/src/pages/menu/MenuPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import MenuContext from '../../context/MenuContext'
 import DynamicNavbar from '../../components/menu/DynamicNavbar'
 import LoadingSpinner from '../../components/LoadingSpinner'
+import MenuItem from '../../components/menu/MenuItem'
 
 function Menu() {
 	const { slug } = useParams()
@@ -13,88 +14,74 @@ function Menu() {
 	let sections = []
 	menuItems.map((item) => (sections.includes(item.section) ? null : sections.push(item.section)))
 
-	if (sections.length < 1) {
+	if (menuItems.length < 1) {
 		return <LoadingSpinner />
 	} else {
 		return (
-			<main className="sm:w-1/2 mx-auto">
-				<section id="restaurant-header" className="w-full mx-auto">
-					<img
-						src={restaurant?.imageUrl}
-						className="object-fill w-full max-h-80 mx-auto"
-						alt="Restaurant Cover"
-					/>
-					<div className="mx-auto max-w-2xl px-3 py-8">
-						<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
-						<p>{restaurant?.description}</p>
-					</div>
-				</section>
-				<DynamicNavbar />
-				{sections.map((section, section_index) => {
-					return (
-						<section className="px-3" key={section_index}>
-							<h2 id={section.replace(' ', '-').toLowerCase()} className="font-bold text-2xl mt-4 pt-4">
-								{section}
-							</h2>
-							<div>
-								{menuItems.map(
-									(item, item_index) =>
-										item.section === section && (
-											<div key={item_index} className="flex border-t pt-4">
-												<div className="mr-auto flex flex-col pr-3">
-													<p className="font-medium text-lg pb-2">{item.name}</p>
-													<p className="py-1">{item.description}</p>
-													<p>
-														{item.categories.map((category, cat_index) => (
-															<span
-																key={cat_index}
-																className="mx-1 text-sm bg-yellow-100 "
-															>
-																<small>{category}</small>
-															</span>
-														))}
-													</p>
-													<p>R${item.price}</p>
-												</div>
-												<img
-													className="h-24 w-40 flex-none rounded object-cover"
-													src={item.imageUrl}
-													alt={item.name}
-												/>
-											</div>
-										)
-								)}
-							</div>
-						</section>
-					)
-				})}
-				<section className="px-3 pb-5">
-					<div className="text-center text-gray-500 border-t">
-						<p className=" mt-5 py-2">
-							{' '}
-							Como podemos melhorar? Deixe-nos uma sugestão{' '}
-							<a className="underline" href="/">
-								aqui
-							</a>
-						</p>
-						<div className="flex justify-center my-1">
-							<a className="px-1" href={restaurant?.instagramUrl}>
-								<img src="/images/icons/instagram_icon.svg" alt="Link to Instagram" />
-							</a>
-							<a className="px-1" href={restaurant?.facebookUrl}>
-								<img src="/images/icons/facebook_icon.svg" alt="Link to Facebook" />
-							</a>
+			<div className="bg-gray-200">
+				<div className="sm:w-1/2 mx-auto">
+					<section id="restaurant-header" className="w-full mx-auto">
+						<img
+							src={restaurant?.imageUrl}
+							className="object-fill w-full max-h-80 mx-auto"
+							alt="Restaurant Cover"
+						/>
+						<div className="mx-auto max-w-2xl px-3 py-8">
+							<h1 className="font-bold text-4xl">{restaurant?.name}</h1>
+							<p>{restaurant?.description}</p>
 						</div>
-						<p className="py-1">
-							Feito com ❤️ por{' '}
-							<a href="https://menuonline.app" target="_blank" rel="noreferrer">
-								menuonline.app
-							</a>
-						</p>
-						<p className="py-1">Copyright ⓒ {new Date().getFullYear()}</p>
-					</div>
-				</section>
-			</main>
+					</section>
+					<DynamicNavbar />
+					{sections.map((section, section_index) => {
+						return (
+							<div className="px-3" key={section_index}>
+								<h2
+									id={section.replace(' ', '-').toLowerCase()}
+									className="font-bold text-2xl mt-4 pt-4"
+								>
+									{section}
+								</h2>
+								<div>
+									{menuItems.map(
+										(item, item_index) =>
+											item.section === section && (
+												<div key={item_index}>
+													<MenuItem item={item} />
+												</div>
+											)
+									)}
+								</div>
+							</div>
+						)
+					})}
+					<section className="px-3 pb-5">
+						<div className="text-center text-gray-500 border-t">
+							<p className=" mt-5 py-2">
+								{' '}
+								Como podemos melhorar? Deixe-nos uma sugestão{' '}
+								<a className="underline" href="/">
+									aqui
+								</a>
+							</p>
+							<div className="flex justify-center my-1">
+								<a className="px-1" href={restaurant?.instagramUrl}>
+									<img src="/images/icons/instagram_icon.svg" alt="Link to Instagram" />
+								</a>
+								<a className="px-1" href={restaurant?.facebookUrl}>
+									<img src="/images/icons/facebook_icon.svg" alt="Link to Facebook" />
+								</a>
+							</div>
+							<p className="py-1">
+								Feito com ❤️ por{' '}
+								<a href="https://menuonline.app" target="_blank" rel="noreferrer">
+									menuonline.app
+								</a>
+							</p>
+							<p className="py-1">Copyright ⓒ {new Date().getFullYear()}</p>
+						</div>
+					</section>
+				</div>
+			</div>
 		)
 	}
 }
