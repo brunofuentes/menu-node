a0290c5fd89e7887f4f304bf8aa5f7c444452ab0
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-10 20:34:29 -0300

    feat: page for non-existing restaurant (wip)

--- client/src/pages/dashboard/MenuItemsPage.jsx
+++ client/src/pages/dashboard/MenuItemsPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ItemsTable from '../../components/dashboard/ItemsTable'
+import { Link } from 'react-router-dom'
 
 function MenuItemsPage() {
+	const restaurant_id = sessionStorage.getItem('restaurant_id')
+
+	if (restaurant_id === 'null' || restaurant_id === 'undefined') {
+		return (
+			<div>
+				Parece que você ainda não tem um restaurante cadastrado. Cadastre{' '}
+				<span className="hover:underline hover:text-blue-600 font-bold">
+					<Link to="/dashboard/restaurant">aqui</Link>
+				</span>
+			</div>
+		)
+	}
+
 	return (
 		<div>
 			<ItemsTable />
