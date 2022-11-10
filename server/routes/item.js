2492220542955b7dc2d8096f80af6406deba7e08
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-10 20:23:32 -0300

    chore: rename middleware from multer to upload

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
+const upload = require('multer')
 const multerConfig = require('../config/multer')
 
 const { createItem, updateItem, getAllItems, getItemsRestaurant, deleteItem, getItem } = require('../controllers/item')
 
-router.post('/items', multer(multerConfig).single('file'), createItem)
+router.post('/items', upload(multerConfig).single('file'), createItem)
 
-router.patch('/items/:id', multer(multerConfig).single('file'), updateItem)
+router.patch('/items/:id', upload(multerConfig).single('file'), updateItem)
 
 router.get('/items', getAllItems)
 
